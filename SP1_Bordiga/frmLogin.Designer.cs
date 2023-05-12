56933ba53fdc519d12a780fb020dd56c7dc14861
Alumnos <Alumnos@PC187>
2023-05-12 12:35:21 -0300

    1235-12052023

--- SP1_Bordiga/frmLogin.Designer.cs
+++ SP1_Bordiga/frmLogin.Designer.cs
@@ -118,6 +118,7 @@ namespace SP1_Bordiga
             // 
             // lstModulo
             // 
+            this.lstModulo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.lstModulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lstModulo.ForeColor = System.Drawing.SystemColors.MenuHighlight;
             this.lstModulo.FormattingEnabled = true;
